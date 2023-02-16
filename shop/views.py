40a459bbf38fa10e0fea06545d2800aa6878c0b0
DavidSaruni <sarunidavid11126@gmail.com>
2023-02-17 01:06:59 +0300

    updateItem view and updateUserOrder

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from .models import *
 
 # Create your views here.
@@ -32,3 +34,7 @@ def checkout(request):
     context = {"items":items, "order":order}
     
     return render(request, 'shop/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
+
